8591fbc995dd3466c71a5bbdab05332f272da480
Alan Soto <ing.alansoto@gmail.com>
2025-09-21 12:21:05 -0500

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/layout/admin/admin.component';
+import { GuestComponent } from './components/layout/guest/guest.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/default',
+        pathMatch: 'full'
+      },
+      {
+        path: 'default',
+        loadComponent: () => import('./pages/dashboard/default/default.component').then((c) => c.DefaultComponent)
+      },
+      {
+        path: 'typography',
+        loadComponent: () => import('./demo/elements/typography/typography.component').then((c) => c.TypographyComponent)
+      },
+      {
+        path: 'color',
+        loadComponent: () => import('./demo/elements/element-color/element-color.component').then((c) => c.ElementColorComponent)
+      },
+      {
+        path: 'sample-page',
+        loadComponent: () => import('./demo/other/sample-page/sample-page.component').then((c) => c.SamplePageComponent)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: GuestComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/authentication/login/login.component').then((c) => c.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/authentication/register/register.component').then((c) => c.RegisterComponent)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
